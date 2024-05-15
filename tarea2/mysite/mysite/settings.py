525228617c5240171f47248703d04bc3a25b0c21
Julian2603 <julian.robertsolano@ucr.ac.cr>
2024-05-15 00:00:25 -0600

    ADD: Complete navigation. Added html and tyling for the blog app

--- tarea2/mysite/mysite/settings.py
+++ tarea2/mysite/mysite/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -46,8 +46,6 @@ INSTALLED_APPS = [
     'about',
     'resume',
 ]
-
-STATIC_URL = "static/"
 
 TAILWIND_APP_NAME = 'theme'
 
@@ -135,6 +133,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
